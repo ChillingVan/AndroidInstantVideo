5a0d61ed91a489d3e33fc5f33a2a3613334132cf
ChillingVan” <chillingvan@163.com>
2017-06-04 10:40:59 +0800

    RTMP publish to server, success

--- applibs/src/main/java/com/chillingvan/lib/muxer/RTMPStreamMuxer.java
+++ applibs/src/main/java/com/chillingvan/lib/muxer/RTMPStreamMuxer.java
@@ -26,6 +26,12 @@ import com.chillingvan.canvasgl.Loggers;
 
 import net.butterflytv.rtmp_client.RTMPMuxer;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
 /**
  * Created by Chilling on 2017/5/29.
  */
@@ -33,11 +39,17 @@ import net.butterflytv.rtmp_client.RTMPMuxer;
 public class RTMPStreamMuxer implements IMuxer {
 
 
+    public static final int KEEP_COUNT = 30;
     private String filename = "";
     private RTMPMuxer rtmpMuxer;
 
-    private long lastVideoTime = -1;
-    private long lastAudioTime = -1;
+    private long lastVideoTime;
+    private long lastAudioTime;
+    private int totalVideoTime;
+    private int totalAudioTime;
+
+
+    private List<Frame> frameQueue = new LinkedList<>();
 
     public RTMPStreamMuxer() {
         this("");
@@ -49,23 +61,28 @@ public class RTMPStreamMuxer implements IMuxer {
     }
 
     @Override
-    public int open(String url, int videoWidth, int videoHeight) {
+    public synchronized int open(String url, int videoWidth, int videoHeight) {
+        lastVideoTime = -1;
+        lastAudioTime = -1;
+        totalVideoTime = 0;
+        totalAudioTime = 0;
+
+        int open = rtmpMuxer.open(url, videoWidth, videoHeight);
+        Loggers.d("RTMPStreamMuxer", String.format(Locale.CHINA, "open: open: %d", open));
+        int connected = rtmpMuxer.isConnected();
+        Loggers.d("RTMPStreamMuxer", String.format(Locale.CHINA, "open: isConnected: %d", connected));
+
         Loggers.d("RTMPStreamMuxer", String.format("open: %s", url));
         if (!"".equals(filename)) {
             rtmpMuxer.file_open(filename);
             rtmpMuxer.write_flv_header(true, true);
         }
-
-        int open = rtmpMuxer.open(url, videoWidth, videoHeight);
-        Loggers.d("RTMPStreamMuxer", String.format("open: open: %d", open));
-        int connected = rtmpMuxer.isConnected();
-        Loggers.d("RTMPStreamMuxer", String.format("open: isConnected: %d", connected));
         return connected;
     }
 
     @Override
-    public void writeVideo(byte[] buffer, int offset, int length, MediaCodec.BufferInfo bufferInfo) {
-        Loggers.d("RTMPStreamMuxer", String.format("writeVideo: "));
+    public synchronized void writeVideo(byte[] buffer, int offset, int length, MediaCodec.BufferInfo bufferInfo) {
+        Loggers.d("RTMPStreamMuxer", "writeVideo: ");
         if (lastVideoTime <= 0) {
             lastVideoTime = bufferInfo.presentationTimeUs;
         }
@@ -73,27 +90,80 @@ public class RTMPStreamMuxer implements IMuxer {
         int delta = (int) (bufferInfo.presentationTimeUs - lastVideoTime);
         lastVideoTime = bufferInfo.presentationTimeUs;
 
-        rtmpMuxer.writeVideo(buffer, offset, length, Math.abs(delta/1000));
+        totalVideoTime += Math.abs(delta/1000);
+        addFrame(buffer, offset, length, totalVideoTime, Frame.TYPE_VIDEO);
+        sendFrame(KEEP_COUNT);
+    }
+
+    private void addFrame(byte[] buffer, int offset, int length, int time, int type) {
+        byte[] frameData = new byte[length];
+        System.arraycopy(buffer, offset, frameData, 0, length);
+        frameQueue.add(new Frame(frameData, time, type));
+        Frame.sortFrame(frameQueue);
+    }
+
+    private void sendFrame(int keepCount) {
+        while (frameQueue.size() >= keepCount) {
+            Frame sendFrame = frameQueue.remove(0);
+            Loggers.i("RTMPStreamMuxer", String.format(Locale.CHINA, "sendFrame: size:%d time:%d, type:%d", sendFrame.data.length, sendFrame.timeStampMs, sendFrame.type));
+            byte[] array = sendFrame.data;
+            if (sendFrame.type == Frame.TYPE_VIDEO) {
+                rtmpMuxer.writeVideo(array, 0, array.length, sendFrame.timeStampMs);
+            } else {
+                rtmpMuxer.writeAudio(array, 0, array.length, sendFrame.timeStampMs);
+            }
+        }
     }
 
     @Override
-    public void writeAudio(byte[] buffer, int offset, int length, MediaCodec.BufferInfo bufferInfo) {
-        Loggers.d("RTMPStreamMuxer", String.format("writeAudio: "));
+    public synchronized void writeAudio(byte[] buffer, int offset, int length, MediaCodec.BufferInfo bufferInfo) {
+        Loggers.d("RTMPStreamMuxer", "writeAudio: ");
         if (lastAudioTime <= 0) {
             lastAudioTime = bufferInfo.presentationTimeUs;
         }
 
         int delta = (int) (bufferInfo.presentationTimeUs - lastAudioTime);
         lastAudioTime = bufferInfo.presentationTimeUs;
-        rtmpMuxer.writeAudio(buffer, offset, length,  Math.abs(delta/1000));
+        totalAudioTime += Math.abs(delta/1000);
+        addFrame(buffer, offset, length, totalAudioTime, Frame.TYPE_AUDIO);
+        sendFrame(KEEP_COUNT);
     }
 
     @Override
-    public int close() {
+    public synchronized int close() {
+        sendFrame(0);
         if (!"".equals(filename)) {
             rtmpMuxer.file_close();
         }
         return rtmpMuxer.close();
-//        return 0;
+    }
+
+    private static class Frame {
+        public byte[] data;
+        public int timeStampMs;
+        public int type;
+        public static final int TYPE_VIDEO = 1;
+        public static final int TYPE_AUDIO = 2;
+
+        public Frame(byte[] data, int timeStampMs, int type) {
+            this.data = data;
+            this.timeStampMs = timeStampMs;
+            this.type = type;
+        }
+
+        static void sortFrame(List<Frame> frameQueue) {
+            Collections.sort(frameQueue, new Comparator<Frame>() {
+                @Override
+                public int compare(Frame left, Frame right) {
+                    if (left.timeStampMs < right.timeStampMs) {
+                        return -1;
+                    } else if (left.timeStampMs == right.timeStampMs) {
+                        return 0;
+                    } else {
+                        return 1;
+                    }
+                }
+            });
+        }
     }
 }
